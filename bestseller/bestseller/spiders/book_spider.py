536ba1b4f4b4bbc55b3915c3b1effd202fdd4caf
ksusavershinina <vershinina.ka@students.dvfu.ru>
2023-08-05 18:35:49 +1000

    зачатки логики в скрейпере - переход на страницу книги со страницы поиска

--- bestseller/bestseller/spiders/book_spider.py
+++ bestseller/bestseller/spiders/book_spider.py
@@ -1,6 +1,9 @@
 import scrapy
 import re
 
+# TODO: пристроить куда то запись в бд/файл???
+
+
 class BookSpider(scrapy.Spider):
     name = 'livelib'
     start_urls = ['https://www.livelib.ru/find']
@@ -10,19 +13,46 @@ class BookSpider(scrapy.Spider):
              '978-5-9500341-0-7',
              '978-9934-8753-1-1',
              '978-5-600-01715-3']
-        for isbn in a:
+        #TODO: как сделать так, чтобы мы шли по исбн и названиям? надо ыпихнуть так, чтобы везде шло. стоит ли исполь-оавть meta?
+        for isbn, title in a:
             link = f"https://www.livelib.ru/find/{isbn}"
-            yield response.follow(link, callback=self.parse_book)
+            yield response.follow(link, callback=self.parse_page, meta={'title': title})
+
+
+    def parse_page(self, response):
+        book_link = response.css('div.find-book-block a::attr(href)').getall()
+
+        if len(book_link) == 1:
+            yield response.follow(book_link[0], callback=self.parse_book)
+        elif len(book_link) == 0:
+            pass
+        elif len(book_link) > 1:
+            book_title = response.css('div.find-book-block a.find-book-name::text').getall()
+            for title, i in book_title:
+                #TODO: форматирование/валидация названия???
+                if title in response.meta['title']:
+                    yield response.follow(book_link[i], callback=self.parse_book)
+                else:
+                    pass
+
 
 
     def parse_book(self, response):
-        description = response.css('div#lenta-card__text-edition-escaped::text').get()
-        description = description.strip()
-        description = re.sub(r'\s+', ' ', description)
+        # description = response.css('div#lenta-card__text-edition-full').get()
 
-        yield {
-            'description': description
-        }
+        description = response.css('div#lenta-card__text-edition-escaped::text').get()
+        #TODO: как получить полное описание???
+        if description:
+            description = description.strip()
+            description = re.sub(r'\s+', ' ', description)
+
+            yield {
+                'description': description
+            }
+        else:
+            yield {
+                'description' : '-'
+            }
 
 
 # response.css("a.slide-book__link::attr(href)").get()
