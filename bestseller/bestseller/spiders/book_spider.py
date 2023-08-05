9f34adac5e24afea9995f1d4fcdd1eabd8f02998
ksusavershinina <vershinina.ka@students.dvfu.ru>
2023-08-05 14:49:05 +1000

    сырой коммит

--- bestseller/bestseller/spiders/book_spider.py
+++ bestseller/bestseller/spiders/book_spider.py
@@ -2,15 +2,11 @@ import scrapy
 
 class BookSpider(scrapy.Spider):
     name = 'livelib'
-    start_urls = ['https://www.livelib.ru/genre/%D0%AD%D0%BA%D0%BE%D0%BD%D0%BE%D0%BC%D0%B8%D0%BA%D0%B0/best/listview/biglist']
+    start_urls = ['https://www.livelib.ru/']
 
     def parse(self, response):
-        for link in response.css('a.brow-book-name.with-cycle::attr(href)'):
+        for link in response.css('a.slide-book__link::attr(href)'):
             yield response.follow(link, callback=self.parse_book)
-
-        for i in range (1, 11):
-            next_page = f'https://www.livelib.ru/genre/%D0%AD%D0%BA%D0%BE%D0%BD%D0%BE%D0%BC%D0%B8%D0%BA%D0%B0/best/listview/biglist/~{i}'
-            yield response.follow(next_page, callback=self.parse)
 
 
     def parse_book(self, response):
@@ -20,3 +16,5 @@ class BookSpider(scrapy.Spider):
             'isbn': response.css('span[itemprop="isbn"]::text').get()
         }
 
+
+# response.css("a.slide-book__link::attr(href)").get()
