6c9e019718f0c83f9bb16bdd7ab677963f877158
bokoAsap <boiko.gd@students.dvfu.ru>
2023-08-05 20:08:26 +1000

    update spider

--- bestseller/bestseller/spiders/book_spider.py
+++ bestseller/bestseller/spiders/book_spider.py
@@ -1,8 +1,9 @@
 import scrapy
+import re
 
 class BookSpider(scrapy.Spider):
     name = 'livelib'
-    start_urls = ['https://www.livelib.ru/']
+    start_urls = ['https://www.livelib.ru/find']
 
     headers = {
         'Host': 'www.livelib.ru',
@@ -20,14 +21,28 @@ class BookSpider(scrapy.Spider):
 
     cookies = '__ddg1_=VDh13kXeUwVWhy8DxvHu; __ll_tum=1877629280; _ga_90RPM8SDHL=GS1.1.1691221633.3.1.1691221743.20.0.0; _ga=GA1.2.360621380.1691146407; __llutmz=-600; __llutmf=0; __ll_fv=1691146409; __ll_dv=1691221741; __ll_cp=6; __ll_popup_count_pviews=mailc1_; _gid=GA1.2.1227887881.1691146424; tmr_lvid=cee4b20ebf3c74fe45044991249f2bf3; tmr_lvidTS=1691146424800; tmr_detect=0%7C1691221748055; __r=pc4340c1_; __ll_ab_mp=1; __ll_unreg_session=dfed4f2ab4423f6fde59ba810f74b88e; __ll_unreg_sessions_count=2; pushsub=1; __gr=g144c1_g137c1_g1240c1_; LiveLibId=dfed4f2ab4423f6fde59ba810f74b88e; __ll_dvs=5; ll_asid=1377305511; promoLLid=tbqammr3dqoe4sk6apue7054b7; showed_vkid_onetap=1; __utnt=g0_y0_a15721_u0_c0'
 
+
     def parse(self, response):
-        for link in response.css('a.slide-book__link::attr(href)'):
-            yield response.follow(link, callback=self.parse_book, headers=self.headers, cookies=self.cookies)
+        a = ['978-5-6044767-4-1',
+             '978-5-6044767-7-2',
+             '978-5-9500341-0-7',
+             '978-9934-8753-1-1',
+             '978-5-600-01715-3']
+        for isbn in a:
+            link = f"https://www.livelib.ru/find/{isbn}"
+            yield response.follow(link, callback=self.parse_book, cookies=self.cookies, headers=self.headers)
 
 
     def parse_book(self, response):
-       yield {
-           'title': response.css('h1.bc__book-title::text').get(),
-           'year': response.css('div.bc-info__wrapper p::text').getall()[1][13:],
-           'isbn': response.css('span[itemprop="isbn"]::text').get()
-       }
+        description = response.css('div#lenta-card__text-edition-escaped::text').get()
+        description = description.strip()
+        description = re.sub(r'\s+', ' ', description)
+
+        yield {
+            'description': description
+        }
+
+
+# response.css("a.slide-book__link::attr(href)").get()
+
+ # response.css('div#lenta-card__text-edition-escaped::text').get()
