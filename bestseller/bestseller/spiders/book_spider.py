569c1b7fa9793e875449fd7c5693eb1585feb255
ksusavershinina <vershinina.ka@students.dvfu.ru>
2023-08-05 18:35:49 +1000

    возможно подающий признаки жизни коммит

--- bestseller/bestseller/spiders/book_spider.py
+++ bestseller/bestseller/spiders/book_spider.py
@@ -1,20 +1,30 @@
 import scrapy
+import re
 
 class BookSpider(scrapy.Spider):
     name = 'livelib'
-    start_urls = ['https://www.livelib.ru/']
-
+    start_urls = ['https://www.livelib.ru/find']
     def parse(self, response):
-        for link in response.css('a.slide-book__link::attr(href)'):
+        a = ['978-5-6044767-4-1',
+             '978-5-6044767-7-2',
+             '978-5-9500341-0-7',
+             '978-9934-8753-1-1',
+             '978-5-600-01715-3']
+        for isbn in a:
+            link = f"https://www.livelib.ru/find/{isbn}"
             yield response.follow(link, callback=self.parse_book)
 
 
     def parse_book(self, response):
+        description = response.css('div#lenta-card__text-edition-escaped::text').get()
+        description = description.strip()
+        description = re.sub(r'\s+', ' ', description)
+
         yield {
-            'title': response.css('h1.bc__book-title::text').get(),
-            'year': response.css('div.bc-info__wrapper p::text').getall()[1][13:],
-            'isbn': response.css('span[itemprop="isbn"]::text').get()
+            'description': description
         }
 
 
 # response.css("a.slide-book__link::attr(href)").get()
+
+ # response.css('div#lenta-card__text-edition-escaped::text').get()
