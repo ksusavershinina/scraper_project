9ecf6c5224a065018acaf71dd0ae6418cb45d79c
ksusavershinina <vershinina.ka@students.dvfu.ru>
2023-08-12 03:13:12 +1000

    add test functional of logs

--- bestseller/bestseller/spiders/book_spider.py
+++ bestseller/bestseller/spiders/book_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import re
-
+import logging
+from scrapy.utils.log import configure_logging
 
 class BookSpider(scrapy.Spider):
     name = 'livelib_test'
@@ -18,10 +19,9 @@ class BookSpider(scrapy.Spider):
         'Sec-Fetch-Site': 'none',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36 Edg/115.0.1901.188',
-        'X-Requested-With': 'XMLHttpRequest'
     }
 
-    custom_cookies = {'pushsetnew': '1', '__ddg1_': 'XR1IzhP6AW7V7Gfjjata',
+    custom_cookies = {'__ddg1_': 'XR1IzhP6AW7V7Gfjjata',
                       'LiveLibId': '4cfcc567dda02071eee09dca94ff232d', '__ll_tum': '579836457', '__llutmz': '-600',
                       '__llutmf': '0', 'll_asid': '1371254432', 'SL_GWPT_Show_Hide_tmp': '1', 'SL_wptGlobTipTmp': '1',
                       '__ll_fv': '1690682303', '__ll_dvs': '5', 'closed_vkid_onetap': '1', '__ll_ab_mp': '1',
@@ -31,6 +31,7 @@ class BookSpider(scrapy.Spider):
                       '__ll_dv': '1691759015'}
 
     def start_requests(self):
+        logging.debug('Spider is starting...')
         isbn_massiv = [
             '978-5-6044767-4-1',
             '978-5-6044767-7-2',
@@ -39,19 +40,21 @@ class BookSpider(scrapy.Spider):
             '978-5-6044767-5-8'
         ]
         for isbn in isbn_massiv:
+            logging.debug('Starting requests...')
             link = f"https://www.livelib.ru/find/{isbn}"
             yield scrapy.Request(url=link, callback=self.parse_page, headers=self.custom_headers, method="GET", cookies=self.custom_cookies)
 
     def parse_page(self, response):
-        # book_links = response.css('.find-book-block a::attr(href)').getall()
-        # if len(book_links) == 0:
-        #     book_links = response.css('.brow-title a::attr(href)').getall()
-        # # надо прописать условия на 0 и больше 1 ссылки
-        # for book_link in book_links:
-        #     yield response.follow(url=book_link, callback=self.parse_book)
-        print(response.text)
+        logging.debug('Parsing page...')
+        book_links = response.css('.find-book-block a::attr(href)').getall()
+        if len(book_links) == 0:
+            book_links = response.css('.brow-title a::attr(href)').getall()
+        # надо прописать условия на 0 и больше 1 ссылки
+        for book_link in book_links:
+            yield response.follow(url=book_link, callback=self.parse_book)
 
     def parse_book(self, response):
+        logging.debug('Parsing book...', )
         description = response.css('#lenta-card__text-edition-escaped::text').get()
         if description:
             description = description.strip()
